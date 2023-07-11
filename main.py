39572747d60295e4d687ddafe8ded882a894c533
Filip Krul <gvarph006@gmail.com>
2023-07-11 15:39:38 +0200

    check if commits work

--- main.py
+++ main.py
@@ -6,3 +6,23 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+
+@app.get("/products/")
+def read_products():
+    raise NotImplementedError  # TODO
+
+
+@app.post("/products/")
+def create_product():
+    raise NotImplementedError  # TODO
+
+
+@app.put("/products/{product_id}")
+def update_product(product_id: int):
+    raise NotImplementedError  # TODO
+
+
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int):
+    raise NotImplementedError  # TODO
