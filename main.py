e17708fd8870f17287292c4a3c45073a1ff2161d
Filip Krul <gvarph006@gmail.com>
2023-07-12 00:47:26 +0200

    got basic crud operations to work

--- main.py
+++ main.py
@@ -1,28 +1,77 @@
-from fastapi import FastAPI
+from uuid import uuid4
+import uuid
+
+from fastapi import FastAPI, HTTPException, Response, status
+from fastapi.encoders import jsonable_encoder
+
+from db import Session
+from models import Product, ProductModel
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+@app.get("/products/", response_model=list[ProductModel], status_code=200)
+async def read_products():
+    try:
+        with Session() as session:
+            all_products = session.query(Product).all()
+
+        # convert to ProductModel
+        models = [
+            ProductModel(
+                id=product.id, name=product.name, description=product.description
+            )
+            for product in all_products
+        ]
+        return models
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.get("/products/")
-def read_products():
-    raise NotImplementedError  # TODO
+@app.post("/products/", response_model=ProductModel, status_code=201)
+def create_product(product: ProductModel):
+    if not product.name or not product.description:
+        raise HTTPException(status_code=400, detail="Invalid product")
+    try:
+        with Session() as session:
+            db_product = Product(**product.dict())
+            session.add(db_product)
+            session.commit()
+            session.refresh(db_product)
+        return db_product
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.post("/products/")
-def create_product():
-    raise NotImplementedError  # TODO
-
-
-@app.put("/products/{product_id}")
-def update_product(product_id: int):
-    raise NotImplementedError  # TODO
+@app.put("/products/{product_id}", status_code=200)
+async def update_product(product_id: uuid.UUID, new_product: ProductModel):
+    try:
+        with Session() as session:
+            session.query(Product).filter(Product.id == product_id).update(
+                {
+                    Product.name: new_product.name,
+                    Product.description: new_product.description,
+                }
+            )
+            session.commit()
+            db_product = session.query(Product).filter(Product.id == product_id).first()
+        print(db_product)
+        return db_product
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.delete("/products/{product_id}")
-def delete_product(product_id: int):
-    raise NotImplementedError  # TODO
+async def delete_product(product_id: int):
+    try:
+        with Session() as session:
+            delete_stmt = (
+                session.query(Product).filter(Product.id == product_id).delete()
+            )
+
+            if not delete_stmt:
+                raise HTTPException(status_code=404, detail="Product not found")
+            session.commit()
+        return status.HTTP_204_NO_CONTENT
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- models.py
+++ models.py
@@ -1,28 +1,44 @@
 from dataclasses import dataclass
+from typing import Optional
+import uuid
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 
 from sqlalchemy.ext.declarative import declarative_base
 
+from pydantic import BaseModel
+
+
 import db
+
 
 Base = declarative_base()
 
 
-@dataclass(slots=True)
+@dataclass
 class Product(Base):
     __tablename__ = "products"
 
-    id = Column(UUID, primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String)
     description = Column(String)
 
+    def __repr__(self):
+        return (
+            f"<Product(id={self.id}, name={self.name}, description={self.description})>"
+        )
 
-@dataclass(slots=True)
+
+class ProductModel(BaseModel):
+    name: str
+    description: str
+    id: Optional[uuid.UUID] = None
+
+
 class Offer(Base):
     __tablename__ = "offers"
 
-    id = Column(UUID, primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     price = Column(Integer)
     items_in_stock = Column(Integer)
     product_id = Column(UUID, ForeignKey("products.id"))
