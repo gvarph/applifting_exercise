ccc0b48ede9a80cbf5bffe26f061c3b3c20ebb53
Filip Krul <gvarph006@gmail.com>
2023-07-11 16:03:52 +0200

    added db script

---
+++ run_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+# Use POSTGRES_PASSWORD from environment variable
+docker run --name dev-postgres -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -p 5432:5432 -d postgres
