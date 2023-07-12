15b42996c676d07eaf085e0f32e976e9c317506b
Filip Krul <gvarph006@gmail.com>
2023-07-13 01:44:58 +0200

    Changed consts to a single database url. Also renamed few things

--- consts.py
+++ consts.py
@@ -3,14 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-postgres_db = os.getenv("POSTGRES_DB")
-postgres_port = os.getenv("POSTGRES_PORT")
-postgres_host = os.getenv("POSTGRES_HOST")
-postgres_user = os.getenv("POSTGRES_USER")
-postgres_password = os.getenv("POSTGRES_PASSWORD")
 
-DATABASE_URL = f"postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_db}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-token_secret = os.getenv("REFRESH_TOKEN_SECRET")
+TOKEN_SECRET = os.getenv("TOKEN_SECRET")
 
-base_url = "https://python.exercise.applifting.cz/api/v1"
+API_URL = os.getenv("API_URL")
