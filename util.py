ffffb0d510dc1db2b185e0e28de399a4262bd83a
Filip Krul <gvarph006@gmail.com>
2023-07-13 02:08:04 +0200

    fixed the debug print funcion

--- util.py
+++ util.py
@@ -1,3 +1,8 @@
-def debug_print(message):
-    filename, line_num, _, _ = traceback.extract_stack()[-2]
-    print(f"[DEBUG] {filename}, line {line_num}: {message}")
+import inspect
+import os
+
+
+def debug_print(*to_print):
+    frame = inspect.currentframe().f_back
+    line_number = frame.f_lineno
+    print(f"\033[34m[DEBUG]\033[0m :{line_number} -", *to_print)
