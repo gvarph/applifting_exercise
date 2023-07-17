39061dd2e9ddfdbde54c9abf4d8487c70a7ed57c
Filip Krul <gvarph006@gmail.com>
2023-07-17 15:03:12 +0200

    added some docstrings

--- src/background.py
+++ src/background.py
@@ -9,22 +9,46 @@ from .offers import fetch_products
 logger = get_logger(__name__)
 
 
+# If we wanted to make this scalable, we should separate this into a separate microservice and use something like celery.
+
+
 class OfferWorker:
+    """
+    A worker class responsible for periodically fetching offers for products.
+
+    Attributes:
+        _is_running (bool): Status flag indicating whether the worker is currently running.
+    """
+
     _is_running = False
 
     @classmethod
     def start(cls):
+        """
+        Starts the OfferWorker. This initiates the periodic fetching of offers.
+        """
         cls._is_running = True
         logger.debug("Starting OfferWorker")
         asyncio.create_task(cls.periodic_fetch_offers())
 
     @classmethod
     def stop(cls):
+        """
+        Stops the OfferWorker. This halts the periodic fetching of offers after the current iteration.
+        """
         logger.debug("Stopping OfferWorker")
         cls._is_running = False  # will stop on the next iteration
 
     @classmethod
     async def periodic_fetch_offers(cls):
+        """
+        Periodically fetches offers for all products from the database. The fetching continues as long as
+        the '_is_running' attribute is set to True. The interval between fetches is determined by the
+        'PERIODIC_FETCH_INTERVAL' environmental variable. If '_is_running' is set to False, the fetching
+        will stop after the current iteration.
+
+        Note: This method is an asynchronous coroutine and must be awaited when called.
+        """
         logger.debug("Starting periodic fetch")
         while cls._is_running:
             with session_scope() as session:
