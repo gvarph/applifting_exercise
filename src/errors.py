c433d76b7e779c04a00835866f356cef631d8157
Filip Krul <gvarph006@gmail.com>
2023-07-17 16:15:12 +0200

    improved error handling

--- src/errors.py
+++ src/errors.py
@@ -1,70 +1,96 @@
-class AuthenticationFailedError(Exception):
+from abc import ABC
+
+# I have made a bit of a mess here, but I don't have the time to clean it up right now.
+# Main problem is that I use the same http request related exceptions while providing and API and requesting another API.
+
+
+class CustomException(Exception, ABC):
+    """Base class for custom exceptions"""
+
+    response_code = 500
+    user_message = "An unexpected error occurred"
+
+    def __init__(self, message="An unexpected error occurred"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class AuthenticationFailedError(CustomException):
     """Exception raised for errors in the authentication process"""
 
+    response_code = 401
+
     def __init__(self, message="Authentication Failed"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class InvalidJwtTokenError(Exception):
+class InvalidJwtTokenError(CustomException):
     """Exception raised for errors in decoding JWT token"""
 
+    response_code = 401
+
     def __init__(self, message="Invalid JWT Token"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class ApiRequestError(Exception):
+class ApiRequestError(CustomException):
     """Exception raised for errors in the API request for token"""
 
+    response_code = 400
+
     def __init__(self, message="API request for token failed"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class OffersFetchError(Exception):
+class OffersFetchError(CustomException):
     """Exception raised for errors in the API request for fetching offers"""
 
+    response_code = 500
+
     def __init__(self, message="API request for fetching offers failed"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class ProductRegistrationError(Exception):
+class ProductRegistrationError(CustomException):
     """Exception raised for errors in the product registration process"""
 
+    response_code = 500
+
     def __init__(self, message="Product registration failed"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class EntityNotFound(Exception):
+class EntityNotFound(CustomException):
     """Exception raised for errors caused by missing entities"""
 
+    response_code = 404
+
     def __init__(self, message="Entity not found"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class InvalidCredentialsException(Exception):
+class InvalidCredentialsException(CustomException):
     """Exception raised for errors caused by invalid credentials"""
 
+    response_code = 401
+
     def __init__(self, message="Invalid credentials"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class EnvironmentVariableNotSet(Exception):
+class EnvironmentVariableNotSet(CustomException):
     """Exception raised for errors caused by missing environment variables"""
 
+    response_code = 500
+
     def __init__(self, message="Environment variable not set"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class InvalidLogin(Exception):
+class InvalidLogin(CustomException):
     """Exception raised for errors caused by failed login"""
 
+    response_code = 401
+
     def __init__(self, message="Login failed"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
--- src/middleware/exception.py
+++ src/middleware/exception.py
@@ -1,19 +1,11 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import Request
 from starlette.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from ..logger import get_logger
 
-from ..errors import (
-    AuthenticationFailedError,
-    InvalidJwtTokenError,
-    ApiRequestError,
-    OffersFetchError,
-    ProductRegistrationError,
-    EntityNotFound,
-    InvalidCredentialsException,
-)
+from ..errors import CustomException
 
 logger = get_logger(__name__)
 
@@ -25,35 +17,12 @@ class ExceptionMiddleware(BaseHTTPMiddleware):
         except StarletteHTTPException as e:
             logger.error(f"Starlette HTTP Exception: {str(e)}")
             response = JSONResponse({"detail": e.detail}, status_code=e.status_code)
-        except AuthenticationFailedError as e:
-            logger.error(f"Authentication Failed Error: {str(e)}")
-            response = JSONResponse({"detail": str(e)}, status_code=401)
-        except InvalidJwtTokenError as e:
-            logger.error(f"Invalid JWT Token Error: {str(e)}")
-            response = JSONResponse({"detail": str(e)}, status_code=401)
-        except ApiRequestError as e:
-            logger.error(f"API Request Error: {str(e)}")
-            response = JSONResponse({"detail": str(e)}, status_code=502)
-        except OffersFetchError as e:
-            logger.error(f"Offers Fetch Error: {str(e)}")
-            response = JSONResponse({"detail": str(e)}, status_code=502)
-        except ProductRegistrationError as e:
-            logger.error(f"Product Registration Error: {str(e)}")
-            response = JSONResponse({"detail": str(e)}, status_code=500)
-        except EntityNotFound as e:
-            logger.error(f"Entity Not Found: {str(e)}")
-            response = JSONResponse({"detail": str(e)}, status_code=404)
-        except InvalidCredentialsException as e:
-            logger.error(f"Invalid Credentials Exception: {str(e)}")
-            response = JSONResponse({"detail": str(e)}, status_code=403)
+        except CustomException as e:  # Catch any custom exception
+            logger.error(f"{e.__class__.__name__}: {str(e)}")
+            response = JSONResponse({"detail": str(e)}, status_code=e.response_code)
         except Exception as e:
             logger.error(f"Unexpected Exception: {str(e)}")
             response = JSONResponse(
                 {"detail": "An unexpected error occurred"}, status_code=500
             )
         return response
-
-
-app = FastAPI()
-
-app.add_middleware(ExceptionMiddleware)
