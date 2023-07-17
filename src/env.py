546640df282f4b88d0d6dbd5794dd189de3ee5a0
Filip Krul <gvarph006@gmail.com>
2023-07-17 15:46:37 +0200

    updated env.py

--- src/env.py
+++ src/env.py
@@ -5,29 +5,25 @@ from .errors import EnvironmentVariableNotSet
 
 load_dotenv(".env")
 
-
-CI = os.getenv("CI", False)
-
-TEST_ENV = CI or os.getenv("TEST_ENV", False)
-
-_DATABASE_URL = os.getenv("DATABASE_URL")
-_TOKEN_SECRET = os.getenv("TOKEN_SECRET")
-
-_JWT_SECRET = os.getenv("JWT_SECRET")
-
-if not _TOKEN_SECRET:
-    raise EnvironmentVariableNotSet("TOKEN_SECRET is not set")
-TOKEN_SECRET = _TOKEN_SECRET
-
-if not _JWT_SECRET:
-    raise EnvironmentVariableNotSet("JWT_SECRET is not set")
-JWT_SECRET = _JWT_SECRET
-
-if not _DATABASE_URL:
-    raise EnvironmentVariableNotSet("DATABASE_URL is not set")
-DATABASE_URL = _DATABASE_URL
-
 # if we are in a test environment, we should be getting the values from pytest.ini
+
+
+def get_required_env_variable(var_name):
+    var = os.getenv(var_name)
+    if not var:
+        raise EnvironmentVariableNotSet(f"{var_name} is not set")
+    return var
+
+
+# not not is a trick to convert any truthy value to True and any falsy value to False
+# This ensures that we can use the the values are booleans
+CI = not not os.getenv("CI", False)
+TEST_ENV = CI or not not os.getenv("TEST_ENV", False)
+
+
+TOKEN_SECRET = get_required_env_variable("TOKEN_SECRET")
+JWT_SECRET = get_required_env_variable("JWT_SECRET")
+DATABASE_URL = get_required_env_variable("DATABASE_URL")
 
 API_URL: str = os.getenv("API_URL", "https://python.exercise.applifting.cz/api/v1")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
