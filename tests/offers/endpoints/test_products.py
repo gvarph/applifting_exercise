517e1bb0e8572b99eeeb848a8971b0c6018b57e8
Filip Krul <gvarph006@gmail.com>
2023-07-22 20:29:07 +0200

    added tests that check if the product products endpoint acutally tests the proper variables

--- tests/offers/endpoints/test_products.py
+++ tests/offers/endpoints/test_products.py
@@ -1,13 +1,22 @@
+import json
 from unittest.mock import patch
 import uuid
 from fastapi.testclient import TestClient
 import pytest
 from src.auth import auth_wrapper
 from src.main import app
-from src.pydantic_models import CreateProductModel, ProductModel
+from src.pydantic_models import CreateProductModel, OfferPriceDiff, ProductModel
 from src.services import ProductService
 
 client = TestClient(app)
+
+test_price_diff = OfferPriceDiff(
+    min=1.0,
+    max=2.0,
+    avg=1.5,
+    median=1.5,
+)
+id = uuid.uuid4()
 
 
 class MockProductService:
@@ -20,6 +29,9 @@ class MockProductService:
         return ProductModel(
             id=uuid.uuid4(), name=data.name, description=data.description
         )
+
+    async def get_price_change(self, product_id: str, from_time: float, to_time: float):
+        return test_price_diff
 
 
 async def auth_wrapper_override(token: str = "token"):
@@ -62,3 +74,52 @@ def test_read_products():
         "name": "test",
         "description": "test",
     }
+
+
+def test_get_price_diff_valid():
+    with patch.object(
+        ProductService, "get_price_change", MockProductService.get_price_change
+    ):
+        response = client.get(
+            f"/products/{id}/price-diff", params={"from_time": 1.0, "to_time": 2.0}
+        )
+
+    assert response.status_code == 200
+    assert response.json() == json.loads(test_price_diff.model_dump_json())
+
+
+def test_get_price_diff_invalid_times():
+    with patch.object(
+        ProductService, "get_price_change", MockProductService.get_price_change
+    ):
+        response = client.get(
+            f"/products/{id}/price-diff", params={"from_time": 2.0, "to_time": 1.0}
+        )
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "Start time cannot be greater than end time",
+    }
+
+
+def test_get_price_diff_valid():
+    with patch.object(
+        ProductService, "get_price_change", MockProductService.get_price_change
+    ):
+        response = client.get(
+            f"/products/{id}/price-diff", params={"from_time": 1.0, "to_time": 2.0}
+        )
+
+    assert response.status_code == 200
+    assert response.json() == json.loads(test_price_diff.model_dump_json())
+
+
+def test_get_price_diff_422():
+    with patch.object(
+        ProductService, "get_price_change", MockProductService.get_price_change
+    ):
+        response = client.get(
+            f"/products/{id}/price-diff", params={"from_time": "a", "to_time": "b"}
+        )
+
+    assert response.status_code == 422
